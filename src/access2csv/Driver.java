15572ac3f37e814e98d1b45a059d65355c0d82e7
Marcos Perez <marcos.pe@gmail.com>
2015-11-11 20:11:34 +0100

    Cleaning files

--- src/access2csv/Driver.java
+++ src/access2csv/Driver.java
@@ -12,123 +12,161 @@ import java.util.List;
 import au.com.bytecode.opencsv.CSVWriter;
 
 import com.healthmarketscience.jackcess.*;
+import org.apache.commons.cli.*;
 
-public class Driver {
-
-	static int export(Database db, String tableName, Writer csv) throws IOException{
+public class Driver
+{
+	static int export(Database db, String tableName, Writer csv) throws IOException
+	{
 		Table table = db.getTable(tableName);
 		String[] buffer = new String[table.getColumnCount()];
 		CSVWriter writer = new CSVWriter(new BufferedWriter(csv));		
 		int rows = 0;
-		try{
-			for(Row row : table){
+		try
+		{
+			for(Row row : table)
+			{
 				int i = 0;
-				for (Object object : row.values()) {
+				for (Object object : row.values())
+				{
 					buffer[i++] = object == null ? null : object.toString();
 				}
 				writer.writeNext(buffer);
 				rows++;
 			}
-		}finally{			
+		}
+		finally
+		{			
 			writer.close();
 		}
 		return rows;
 	}
 
-	static void export(String filename, String tableName) throws IOException{
-		Database db = DatabaseBuilder.open(new File(filename));
-		try{
-			export(db, tableName, new PrintWriter(System.out));
-		}finally{
-			db.close();
-		}
-	}
-
-	static void schema(String filename) throws IOException{
-
-		Database db = DatabaseBuilder.open(new File(filename));
-		try{
-			for(String tableName : db.getTableNames()){
+	static void schema(Database db) throws IOException
+	{
+		try
+		{
+			for(String tableName : db.getTableNames())
+			{
 				Table table = db.getTable(tableName);
 				System.out.println(String.format("CREATE TABLE %s (", tableName));
-				for(Column col : table.getColumns()){
+				for(Column col : table.getColumns())
+				{
 					System.out.println(String.format("  %s %s,", 
 							col.getName(), col.getType()));
 				}
 				System.out.println(")");
 			}
-		}finally{
+		}
+		finally
+		{
 			db.close();
 		}
 
-	}
-
-	static void exportAll(String filename) throws IOException{
-		Database db = DatabaseBuilder.open(new File(filename));
-		try{
-			for(String tableName : db.getTableNames()){
-				String csvName = tableName + ".csv";
-				Writer csv = new FileWriter(csvName);
-				try{
-					System.out.println(String.format("Exporting '%s' to %s/%s",
-							tableName, System.getProperty("user.dir"), csvName));
-					int rows = export(db, tableName, csv);
-					System.out.println(String.format("%d rows exported", rows));
-				}finally{
-					try{
-						csv.flush();
-						csv.close();
-					}catch(IOException ex){}
-				}
-			}
-		}finally{
-			db.close();
-		}
-
-	}
-
-	static void printUsage(){
-		System.out.println("Usage:");		
-		System.out.println(" java -jar access2csv.jar [ACCESS FILE] [OPTIONS]");
-		System.out.println("");
-		System.out.println("Options:");
-		System.out.println("");
-		System.out.println(" * if no options are provided, all tables will be exported to CSV files,");
-		System.out.println("   one file per table. Output file paths will be printed to stdout");
-		System.out.println(" * '--schema' - prints the database schema");
-		System.out.println(" * [TABLENAME] - prints the given table as CSV to stdout");
 	}
 
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws IOException {
-		List<String> helpCommands = Arrays.asList(new String[]{"-h", "--help", "-H", "/?"});
+	public static void main(String[] args) throws IOException
+	{
+		String header="Application to read contents from a MS Access file";
+		String footer="";
+		Options options = new Options();
+		options.addOption(new Option("h",//Short option
+				"help", //Long option
+				false, //No extra argumets
+				"Show help"
+				));
+		options.addOption(new Option("s",//Short option
+				"schema", //Long option
+				false, //No extra argumets
+				"Create schema definitions"
+				));
+		options.addOption(new Option("i",//Short option
+				"db", //Long option
+				true, //Extra argumets
+				"Access DB to open"
+				));
+		options.addOption(new Option("t",//Short option
+				"tables", //Long option
+				false, //Extra argumets
+				"Tables to extract"
+				));
+		options.addOption(new Option("p",//Short option
+				"pass", //Long option
+				true, //Extra argumets
+				"Password to open file"
+				));
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd=null;
+		try
+		{
+			cmd = parser.parse(options, args);
+		}
+		catch(ParseException e)
+		{
+			System.err.println("Error parsing arguments");
+			HelpFormatter formatter=new HelpFormatter();
+			formatter.printHelp("access2csv", header, options, footer, true);
+			System.exit(0);
+		}
 
-		if(args.length == 1 && helpCommands.contains(args[0])){			
-			printUsage();
+		if(cmd.hasOption("h")||cmd.hasOption("help"))
+		{
+			HelpFormatter formatter=new HelpFormatter();
+			formatter.printHelp("access2csv", header, options, footer, true);
 			System.exit(0);
 		}
-		if(args.length == 1 && args[0].equals("--schema")){
-			exportAll(args[0]);
-			System.exit(0);
+		String dbFile=null;
+		if(cmd.hasOption("i"))
+			dbFile=cmd.getOptionValue("i");
+		else if(cmd.hasOption("db"))
+			dbFile=cmd.getOptionValue("db");
+		else
+		{
+			System.out.println("Error, no Access DB supplied");
+			HelpFormatter formatter=new HelpFormatter();
+			formatter.printHelp("access2csv", header, options, footer, true);
+			System.exit(-1);
 		}
-		if(args.length == 1){
-			exportAll(args[0]);
-			System.exit(0);
+		boolean exportSchema=cmd.hasOption("s")||cmd.hasOption("schema");
+		String password=null;
+		if(cmd.hasOption("p"))
+			password=cmd.getOptionValue("p");
+		else if(cmd.hasOption("pass"))
+			password=cmd.getOptionValue("pass");
+		boolean exportTables=false;
+
+		if(cmd.hasOption("t")||cmd.hasOption("tables"))
+			exportTables=true;
+
+		Database db = null;
+		try
+		{
+			if(password==null)
+				db=new DatabaseBuilder(new File(dbFile)).setCodecProvider(new CryptCodecProvider()).open();
+			else
+				db=new DatabaseBuilder(new File(dbFile)).setCodecProvider(new CryptCodecProvider(password)).open();
+
+			if(exportSchema)
+				schema(db);
+			if(exportTables)
+			{
+				PrintWriter writer=new PrintWriter(System.out);
+				for(int i=0;i<cmd.getArgs().length;i++)
+					export(db, cmd.getArgs()[i], writer);
+			}
 		}
-		else if(args.length == 2 && args[1].equals("--schema")){
-			schema(args[0]);
-			System.exit(0);
+		finally
+		{
+			if(db!=null)
+				try
+				{
+					db.close();
+				}
+				catch(Exception e) {}
 		}
-		else if(args.length == 2){
-			export(args[0], args[1]);
-			System.exit(0);
-		}
-		System.err.println("Invalid arguments.");
-		printUsage();		
-		System.exit(1);
 	}
-
 }
